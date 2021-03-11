6d3f48cf9b01c4f03bc7a39f53fd4ee99ad7459b
Shuruder <gabriel_brandini@hotmail.com>
2021-03-10 21:02:28 -0300

    ajuste na tipagem dinamica

---
+++ tipagem-dinamica/2-tipagem-dinamica.java
@@ -0,0 +1,9 @@
+public class TipagemDinamica {
+    public static void main(String[] args) {
+        int meuNumero = 10;
+        meuNumero = "texto";
+        
+        System.out.println(meuNumero); 
+    }
+
+}
--- tipagem-dinamica/2-tipagem-dinamica.js
+++ tipagem-dinamica/2-tipagem-dinamica.js
@@ -1,3 +1,5 @@
+// Na tipagem dinamica não há necessidade de declarar o tipo da variavel
+// Assim que atribuimos o valor a ela, o compilador já determina o tipo por inferência
 var minhaVariavel = 30;
 
 minhaVariavel = 'texto';
